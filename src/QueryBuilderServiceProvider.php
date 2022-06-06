b6e5c0c9d869d7a3f0a87eee9877ee8bb7439e1b
osama <devtutegy1@gmail.com>
2022-06-06 14:58:10 +0200

    cleanup

--- src/QueryBuilderServiceProvider.php
+++ src/QueryBuilderServiceProvider.php
@@ -4,22 +4,13 @@ namespace Wpzag\QueryBuilder;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Wpzag\QueryBuilder\Commands\QueryBuilderCommand;
 
 class QueryBuilderServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-query-builder')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-query-builder_table')
-            ->hasCommand(QueryBuilderCommand::class);
+            ->hasConfigFile();
     }
 }
