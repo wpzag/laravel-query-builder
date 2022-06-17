0c2087878df019a34c7340d6021c33c2a36b85d6
osama <devtutegy1@gmail.com>
2022-06-17 11:37:14 +0200

    add createModels function

--- tests/Pest.php
+++ tests/Pest.php
@@ -22,3 +22,17 @@
         return $this->toBeGreaterThanOrEqual($min)
             ->toBeLessThanOrEqual($max);
     });
+
+
+    function createModels(): void
+    {
+        TestModel::factory(5)
+            ->sequence(
+                fn ($sequence) => ['name' => 'one', 'age' => 10, 'created_at' => '2020-01-01 01:00:00'],
+                ['name' => 'two', 'age' => 20, 'created_at' => '2020-02-02 02:00:00'],
+                ['name' => 'three', 'age' => 30, 'created_at' => '2020-03-03 03:00:00'],
+                ['name' => 'four', 'age' => 40, 'created_at' => '2020-04-04 04:00:00'],
+                ['name' => 'five', 'age' => 50, 'created_at' => '2020-05-05 05:00:00']
+            )
+            ->create();
+    }
