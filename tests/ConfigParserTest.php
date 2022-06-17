7f8189801b16b6ca14057fa6e2bf249a4f5128b1
osama <devtutegy1@gmail.com>
2022-06-17 11:38:38 +0200

    modify the config parser test

---
+++ tests/ConfigParserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+    use Wpzag\QueryBuilder\Services\ConfigParser;
+
+    it('can parse filterable columns', function (array $config, array $expected) {
+        setConfig('filterable', $config);
+        ConfigParser::parse();
+        expect(getConfig('filterable'))->toEqualCanonicalizing($expected);
+    })->with([
+        [['name', 'relatedModels.name'], ['name', 'relatedModels.name'],],
+        [['*'], ['id', 'name', 'age', 'common', 'is_visible', 'created_at', 'updated_at']],
+        [['*:except:id,excepted,created_at,updated_at'], ['name', 'age', 'common', 'is_visible']],
+        [['*:except:id,created_at,updated_at', 'relatedModels.name'], ['name', 'age', 'common', 'is_visible', 'relatedModels.name']],
+    ]);
