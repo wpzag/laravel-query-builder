0ad402899c796e6f352c01516d0331ba3c5efecc
wpzag <egypt.zag.eg@gmail.com>
2023-02-18 23:24:04 +0200

    deps bump

--- tests/FilterTest.php
+++ tests/FilterTest.php
@@ -1,170 +1,170 @@
 <?php
-
-    // Creates 5 TestModels
-    use Carbon\Carbon;
-    use function Pest\Laravel\getJson;
-    use Wpzag\QueryBuilder\QueryBuilder;
-    use Wpzag\QueryBuilder\Tests\TestClasses\Models\TestModel;
-
-    beforeEach(fn () => createModels());
-
-    it('wont filter if the filter param is missing')->getJson('/test')->assertJsonCount(5);
-    it('wont filter if the column name is missing')->getJson('/test?filter')->assertJsonCount(5);
-    it('wont filter if the column has no values')->getJson('/test?filter[name]=')->assertJsonCount(5);
-    it('wont filter if the column is not allowed in the config', function () {
-        config(['query-builder.disable_invalid_filter_query_exception' => true]);
-        getJson('/test?filter[radom]=one')->assertJsonCount(5);
-    });
-
-    it('can filter by a single column and single value')
-        ->getJson('/test?filter[name]=one')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['name' => 'one']);
-
-    it('can filter by a single column and multiple values')
-        ->getJson('/test?filter[name]=one,two,three')
-        ->assertJsonCount(3)
-        ->assertJsonFragment(['name' => 'one'])
-        ->assertJsonFragment(['name' => 'two'])
-        ->assertJsonFragment(['name' => 'three']);
-
-    it('can filter by multiple columns and single values')
-        ->getJson('/test?filter[common]=common&filter[age]=10')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['age' => 10]);
-
-    it('can filter by multiple columns for same value')
+	
+	// Creates 5 TestModels
+	use Carbon\Carbon;
+	use Wpzag\QueryBuilder\QueryBuilder;
+	use Wpzag\QueryBuilder\Tests\TestClasses\Models\TestModel;
+	use function Pest\Laravel\getJson;
+	
+	beforeEach(fn () => createModels());
+	
+	it('wont filter if the filter param is missing')->getJson('/test')->assertJsonCount(5);
+	it('wont filter if the column name is missing')->getJson('/test?filter')->assertJsonCount(5);
+	it('wont filter if the column has no values')->getJson('/test?filter[name]=')->assertJsonCount(5);
+	it('wont filter if the column is not allowed in the config', function() {
+		config(['query-builder.disable_invalid_filter_query_exception' => true]);
+		getJson('/test?filter[radom]=one')->assertJsonCount(5);
+	});
+	
+	it('can filter by a single column and single value')
+		->getJson('/test?filter[name]=one')
+		->assertJsonCount(1)
+		->assertJsonFragment(['name' => 'one']);
+	
+	it('can filter by a single column and multiple values')
+		->getJson('/test?filter[name]=one,two,three')
+		->assertJsonCount(3)
+		->assertJsonFragment(['name' => 'one'])
+		->assertJsonFragment(['name' => 'two'])
+		->assertJsonFragment(['name' => 'three']);
+	
+	it('can filter by multiple columns and single values')
+		->getJson('/test?filter[common]=common&filter[age]=10')
+		->assertJsonCount(1)
+		->assertJsonFragment(['age' => 10]);
+	
+	it('can filter by multiple columns for same value')
 //        ->getJson('/test?filter[common,age]=20')
-        ->getJson('/test?filter[common,name]=20,50&filter[age]=20')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['age' => 20]);
-
-    it('can filter by multiple columns and multiple values')
-        ->getJson('/test?filter[common]=common&filter[age]=10,20')
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 10])
-        ->assertJsonFragment(['age' => 20]);
-
-    it('can filter with an greater than operator')
-        ->getJson('/test?filter[age]=gt.30')
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 40])
-        ->assertJsonFragment(['age' => 50]);
-
-
-    it('can filter with an greater than or equal operator')
-        ->getJson('/test?filter[age]=gte.40')
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 40])
-        ->assertJsonFragment(['age' => 50]);
-
-    it('can filter with an less than operator')
-        ->getJson('/test?filter[age]=lt.30')
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 10])
-        ->assertJsonFragment(['age' => 20]);
-
-    it('can filter with an less than or equal operator')
-        ->getJson('/test?filter[age]=lte.20')
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 10])
-        ->assertJsonFragment(['age' => 20]);
-
-    it('can filter with an not equal operator')
-        ->getJson('/test?filter[age]=ne.10')
-        ->assertJsonCount(4)
-        ->assertJsonFragment(['age' => 20])
-        ->assertJsonFragment(['age' => 30])
-        ->assertJsonFragment(['age' => 40])
-        ->assertJsonFragment(['age' => 50]);
-
-    it('can filter with an operator and multiple columns')
-        ->getJson('/test?filter[name]=one&filter[age]=lt.30')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['name' => 'one']);
-
-    it('can filter with an operator and multiple values')
-        ->getJson('/test?filter[age]=lt.30,50')
-        ->assertJsonCount(3)
-        ->assertJsonFragment(['age' => 10])
-        ->assertJsonFragment(['age' => 20])
-        ->assertJsonFragment(['age' => 50]);
-
-    it('can filter between two values')
-        ->getJson('/test?filter[age][between]=[35,50]')
-        ->assertSuccessful()
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 40])
-        ->assertJsonFragment(['age' => 50]);
-
-    it('can filter not between two values')
-        ->getJson('/test?filter[age][not-between]=[20,50]')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['age' => 10]);
-
-    it('can filter in a list')
-        ->getJson('/test?filter[age][in]=[10,20,30]')
-        ->assertJsonCount(3)
-        ->assertJsonFragment(['age' => 10])
-        ->assertJsonFragment(['age' => 20])
-        ->assertJsonFragment(['age' => 30]);
-
-    it('can filter not in a list')
-        ->getJson('/test?filter[age][not-in]=[10,20,30]')
-        ->assertJsonCount(2)
-        ->assertJsonFragment(['age' => 40])
-        ->assertJsonFragment(['age' => 50]);
-
-    it('can filter by date')
-        ->getJson('/test?filter[created_at][date]=2020-01-01')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['created_at' => Carbon::parse('2020-01-01 01:00:00')->toISOString()]);
-
-    it('can filter by  time')
-        ->getJson('/test?filter[created_at][time]=02:00:00')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['created_at' => Carbon::parse('2020-02-02 02:00:00')->toISOString()]);
-
-    it('can filter by day')
-        ->getJson('/test?filter[created_at][day]=2')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['created_at' => Carbon::parse('2020-02-02 02:00:00')->toISOString()]);
-
-    it('can filter by month')
-        ->getJson('/test?filter[created_at][month]=2')
-        ->assertJsonCount(1)
-        ->assertJsonFragment(['created_at' => Carbon::parse('2020-02-02 02:00:00')->toISOString()]);
-
-    it('can filter by year')
-        ->getJson('/test?filter[created_at][year]=2020')
-        ->assertJsonCount(5);
-
-    it('can filter by a null columns', function () {
-        TestModel::find(1)->update(['age' => null]);
-        $this->getJson('/test?filter[age][empty]')->assertJsonCount(1);
-    });
-
-    it('can filter by a none-null columns', function () {
-        TestModel::query()->update(['age' => null]);
-        TestModel::find(1)->update(['age' => 10]);
-        $this->getJson('/test?filter[age][empty]')->assertJsonCount(4);
-    });
-
-    it('wont ignore falsy values', function () {
-        $this->getJson('/test?filter[age]=0')
-            ->assertJsonCount(0);
-    });
-
-    it('can filter by related model', function () {
-        TestModel::find(1)->relatedModels()->create(['name' => 'one']);
-        $this->getJson('/test?filter[relatedModels.name]=one')
-            ->assertJsonCount(1);
-    });
-    it('can filter by nested related model', function () {
-        $related = TestModel::find(1)->relatedModels()->create(['name' => 'one']);
-        request()->query->set('filter[relatedModels.relatedModels.name]', 'one');
-        QueryBuilder::for(TestModel::class);
-        $related->nestedRelatedModels()->create(['name' => 'nested']);
-        $this->getJson('/test?filter[relatedModels.nestedRelatedModels.name]=dfs')
-            ->assertJsonCount(1);
-    });
+		->getJson('/test?filter[common,name]=20,50&filter[age]=20')
+		->assertJsonCount(1)
+		->assertJsonFragment(['age' => 20]);
+	
+	it('can filter by multiple columns and multiple values')
+		->getJson('/test?filter[common]=common&filter[age]=10,20')
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 10])
+		->assertJsonFragment(['age' => 20]);
+	
+	it('can filter with an greater than operator')
+		->getJson('/test?filter[age]=gt.30')
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 40])
+		->assertJsonFragment(['age' => 50]);
+	
+	
+	it('can filter with an greater than or equal operator')
+		->getJson('/test?filter[age]=gte.40')
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 40])
+		->assertJsonFragment(['age' => 50]);
+	
+	it('can filter with an less than operator')
+		->getJson('/test?filter[age]=lt.30')
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 10])
+		->assertJsonFragment(['age' => 20]);
+	
+	it('can filter with an less than or equal operator')
+		->getJson('/test?filter[age]=lte.20')
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 10])
+		->assertJsonFragment(['age' => 20]);
+	
+	it('can filter with an not equal operator')
+		->getJson('/test?filter[age]=ne.10')
+		->assertJsonCount(4)
+		->assertJsonFragment(['age' => 20])
+		->assertJsonFragment(['age' => 30])
+		->assertJsonFragment(['age' => 40])
+		->assertJsonFragment(['age' => 50]);
+	
+	it('can filter with an operator and multiple columns')
+		->getJson('/test?filter[name]=one&filter[age]=lt.30')
+		->assertJsonCount(1)
+		->assertJsonFragment(['name' => 'one']);
+	
+	it('can filter with an operator and multiple values')
+		->getJson('/test?filter[age]=lt.30,50')
+		->assertJsonCount(3)
+		->assertJsonFragment(['age' => 10])
+		->assertJsonFragment(['age' => 20])
+		->assertJsonFragment(['age' => 50]);
+	
+	it('can filter between two values')
+		->getJson('/test?filter[age][between]=[35,50]')
+		->assertSuccessful()
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 40])
+		->assertJsonFragment(['age' => 50]);
+	
+	it('can filter not between two values')
+		->getJson('/test?filter[age][not-between]=[20,50]')
+		->assertJsonCount(1)
+		->assertJsonFragment(['age' => 10]);
+	
+	it('can filter in a list')
+		->getJson('/test?filter[age][in]=[10,20,30]')
+		->assertJsonCount(3)
+		->assertJsonFragment(['age' => 10])
+		->assertJsonFragment(['age' => 20])
+		->assertJsonFragment(['age' => 30]);
+	
+	it('can filter not in a list')
+		->getJson('/test?filter[age][not-in]=[10,20,30]')
+		->assertJsonCount(2)
+		->assertJsonFragment(['age' => 40])
+		->assertJsonFragment(['age' => 50]);
+	
+	it('can filter by date')
+		->getJson('/test?filter[created_at][date]=2020-01-01')
+		->assertJsonCount(1)
+		->assertJsonFragment(['created_at' => Carbon::parse('2020-01-01 01:00:00')->toISOString()]);
+	
+	it('can filter by  time')
+		->getJson('/test?filter[created_at][time]=02:00:00')
+		->assertJsonCount(1)
+		->assertJsonFragment(['created_at' => Carbon::parse('2020-02-02 02:00:00')->toISOString()]);
+	
+	it('can filter by day')
+		->getJson('/test?filter[created_at][day]=2')
+		->assertJsonCount(1)
+		->assertJsonFragment(['created_at' => Carbon::parse('2020-02-02 02:00:00')->toISOString()]);
+	
+	it('can filter by month')
+		->getJson('/test?filter[created_at][month]=2')
+		->assertJsonCount(1)
+		->assertJsonFragment(['created_at' => Carbon::parse('2020-02-02 02:00:00')->toISOString()]);
+	
+	it('can filter by year')
+		->getJson('/test?filter[created_at][year]=2020')
+		->assertJsonCount(5);
+	
+	it('can filter by a null columns', function() {
+		TestModel::find(1)->update(['age' => null]);
+		$this->getJson('/test?filter[age][empty]')->assertJsonCount(1);
+	});
+	
+	it('can filter by a none-null columns', function() {
+		TestModel::query()->update(['age' => null]);
+		TestModel::find(1)->update(['age' => 10]);
+		$this->getJson('/test?filter[age][empty]')->assertJsonCount(4);
+	});
+	
+	it('wont ignore falsy values', function() {
+		$this->getJson('/test?filter[age]=0')
+			->assertJsonCount(0);
+	});
+	
+	it('can filter by related model', function() {
+		TestModel::find(1)->relatedModels()->create(['name' => 'one']);
+		$this->getJson('/test?filter[relatedModels.name]=one')
+			->assertJsonCount(1);
+	});
+	it('can filter by nested related model', function() {
+		$related = TestModel::find(1)->relatedModels()->create(['name' => 'one']);
+		request()->query->set('filter[relatedModels.relatedModels.name]', 'one');
+		QueryBuilder::for(TestModel::class);
+		$related->nestedRelatedModels()->create(['name' => 'nested']);
+		$this->getJson('/test?filter[relatedModels.nestedRelatedModels.name]=dfs')
+			->assertJsonCount(1);
+	});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,135 +1,135 @@
 <?php
-
-    namespace Wpzag\QueryBuilder\Tests;
-
-    use Illuminate\Database\Eloquent\Factories\Factory;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Foundation\Application;
-    use Illuminate\Foundation\Testing\DatabaseMigrations;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
-    use Orchestra\Testbench\TestCase as Orchestra;
-    use Wpzag\QueryBuilder\QueryBuilder;
-    use Wpzag\QueryBuilder\QueryBuilderServiceProvider;
-    use Wpzag\QueryBuilder\Tests\TestClasses\Models\TestModel;
-
-    class TestCase extends Orchestra
-    {
-        use DatabaseMigrations;
-
-        protected function setUp(): void
-        {
-            parent::setUp();
-            $this->setUpConfig();
-            $this->setUpDatabase($this->app);
-            Factory::guessFactoryNamesUsing(
-                fn (string $modelName) => 'Wpzag\\QueryBuilder\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
-            );
-            $this->setUpRoutes();
-        }
-
-        protected function getPackageProviders($app): array
-        {
-            return [
-                QueryBuilderServiceProvider::class,
-            ];
-        }
-
-        protected function setUpDatabase(Application $app)
-        {
-            $getSchemaBuilder = $app[ 'db' ]->connection()->getSchemaBuilder();
-            $getSchemaBuilder->create('test_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('name')->nullable();
-                $table->integer('age')->nullable();
-                $table->string('common')->default('common');
-                $table->boolean('is_visible')->default(true);
-                $table->timestamps();
-            });
-
-            $getSchemaBuilder->create('append_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('firstname');
-                $table->string('lastname');
-            });
-
-            $getSchemaBuilder->create('soft_delete_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->softDeletes();
-                $table->string('name');
-            });
-
-            $getSchemaBuilder->create('scope_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('name');
-            });
-
-            $getSchemaBuilder->create('related_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->integer('test_model_id');
-                $table->string('name');
-            });
-
-            $getSchemaBuilder->create('nested_related_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->integer('related_model_id');
-                $table->string('name');
-            });
-
-            $getSchemaBuilder->create('pivot_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('test_model_id');
-                $table->integer('related_through_pivot_model_id');
-                $table->string('location')->nullable();
-            });
-
-            $getSchemaBuilder->create('related_through_pivot_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->string('name');
-            });
-
-            $getSchemaBuilder->create('morph_models', function (Blueprint $table) {
-                $table->increments('id');
-                $table->morphs('parent');
-                $table->string('name');
-            });
-        }
-
-        private function setUpRoutes()
-        {
-            Route::get('/test', function (Request $request) {
-                $builder = QueryBuilder::for(TestModel::class);
-                if ($request->debug) {
-                    $builder->query()->dd();
-                }
-                if ($request->append && $request->page) {
-                    $response = $builder->withPaginationAndAppends();
-                } elseif ($request->page) {
-                    $response = $builder->withPagination();
-                } elseif ($request->append) {
-                    $response = $builder->withAppends();
-                } else {
-                    $response = $builder->get();
-                }
-
-                return response()->json($response);
-            });
-        }
-
-        private function setUpConfig()
-        {
-            config(['query-builder.models' => [
-                TestModel::class => [
-                    'includes' => ['relatedModels', "relatedModels.nestedRelatedModels"],
-                    'appends' => ['appended_field'],
-                    'filterable' => ['name', 'age:exact', 'common', 'null_field', 'created_at', 'relatedModels.name', 'relatedModels.nestedRelatedModels.name'],
-                    'sortable' => ['name', 'age', 'created_at'],
-                    'max_per_page' => 10,
-                ],],
-            ]);
-            config(['query-builder.disable_invalid_filter_query_exception' => false]);
-            config(['query-builder.disable_invalid_sort_exception' => false]);
-            config(['query-builder.disable_invalid_include_query_exception' => false]);
-            config(['query-builder.disable_invalid_appends_exception' => false]);
-        }
-    }
+	
+	namespace Wpzag\QueryBuilder\Tests;
+	
+	use Illuminate\Database\Eloquent\Factories\Factory;
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Foundation\Application;
+	use Illuminate\Foundation\Testing\DatabaseMigrations;
+	use Illuminate\Http\Request;
+	use Illuminate\Support\Facades\Route;
+	use Orchestra\Testbench\TestCase as Orchestra;
+	use Wpzag\QueryBuilder\QueryBuilder;
+	use Wpzag\QueryBuilder\QueryBuilderServiceProvider;
+	use Wpzag\QueryBuilder\Tests\TestClasses\Models\TestModel;
+	
+	class TestCase extends Orchestra
+	{
+		use DatabaseMigrations;
+		
+		protected function setUp() : void
+		{
+			parent::setUp();
+			$this->setUpConfig();
+			$this->setUpDatabase($this->app);
+			Factory::guessFactoryNamesUsing(
+				fn (string $modelName) => 'Wpzag\\QueryBuilder\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+			);
+			$this->setUpRoutes();
+		}
+		
+		protected function getPackageProviders($app) : array
+		{
+			return [
+				QueryBuilderServiceProvider::class,
+			];
+		}
+		
+		protected function setUpDatabase(Application $app)
+		{
+			$getSchemaBuilder = $app[ 'db' ]->connection()->getSchemaBuilder();
+			$getSchemaBuilder->create('test_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('name')->nullable();
+				$table->integer('age')->nullable();
+				$table->string('common')->default('common');
+				$table->boolean('is_visible')->default(true);
+				$table->timestamps();
+			});
+			
+			$getSchemaBuilder->create('append_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('firstname');
+				$table->string('lastname');
+			});
+			
+			$getSchemaBuilder->create('soft_delete_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->softDeletes();
+				$table->string('name');
+			});
+			
+			$getSchemaBuilder->create('scope_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('name');
+			});
+			
+			$getSchemaBuilder->create('related_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->integer('test_model_id');
+				$table->string('name');
+			});
+			
+			$getSchemaBuilder->create('nested_related_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->integer('related_model_id');
+				$table->string('name');
+			});
+			
+			$getSchemaBuilder->create('pivot_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('test_model_id');
+				$table->integer('related_through_pivot_model_id');
+				$table->string('location')->nullable();
+			});
+			
+			$getSchemaBuilder->create('related_through_pivot_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('name');
+			});
+			
+			$getSchemaBuilder->create('morph_models', function(Blueprint $table) {
+				$table->increments('id');
+				$table->morphs('parent');
+				$table->string('name');
+			});
+		}
+		
+		private function setUpRoutes()
+		{
+			Route::get('/test', function(Request $request) {
+				$builder = QueryBuilder::for(TestModel::class);
+				if($request->debug) {
+					$builder->query()->dd();
+				}
+				if($request->append && $request->page) {
+					$response = $builder->withPaginationAndAppends();
+				} elseif($request->page) {
+					$response = $builder->withPagination();
+				} elseif($request->append) {
+					$response = $builder->withAppends();
+				} else {
+					$response = $builder->get();
+				}
+				
+				return response()->json($response);
+			});
+		}
+		
+		private function setUpConfig()
+		{
+			config(['query-builder.models' => [
+				TestModel::class => [
+					'includes' => ['relatedModels', "relatedModels.nestedRelatedModels"],
+					'appends' => ['appended_field'],
+					'filterable' => ['name', 'age:exact', 'common', 'null_field', 'created_at:exact', 'relatedModels.name', 'relatedModels.nestedRelatedModels.name'],
+					'sortable' => ['name', 'age', 'created_at'],
+					'max_per_page' => 10,
+				],],
+			]);
+			config(['query-builder.disable_invalid_filter_query_exception' => false]);
+			config(['query-builder.disable_invalid_sort_exception' => false]);
+			config(['query-builder.disable_invalid_include_query_exception' => false]);
+			config(['query-builder.disable_invalid_appends_exception' => false]);
+		}
+	}
